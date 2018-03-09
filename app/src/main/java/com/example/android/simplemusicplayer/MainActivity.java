11bf305bae10552956a3fcea34ec1e2fd894d057
Erni <31850356+KajcsaErno@users.noreply.github.com>
2018-03-09 17:07:27 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/android/simplemusicplayer/MainActivity.java
+++ app/src/main/java/com/example/android/simplemusicplayer/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
 
     private ImageView playIcon, plusIcon, minusIcon, skipPreviousIcon, skipNextIcon, fastForwardIcon, fastRewindIcon;
     private SeekBar seekBar;
+    private TextView beginingTextViwe, endTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,17 +25,47 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         playIcon = findViewById(R.id.play_icon);
-        minusIcon = findViewById(R.id.minus_icon);
+        beginingTextViwe = findViewById(R.id.begining__text_view);
+        endTextView = findViewById(R.id.end_text_view);
+        seekBar = findViewById(R.id.seek_bar);
+
         plusIcon = findViewById(R.id.plus_icon);
+        minusIcon = findViewById(R.id.minus_icon);
+
         skipPreviousIcon = findViewById(R.id.skip_previous_icon);
         skipNextIcon = findViewById(R.id.skip_next_icon);
+
         fastForwardIcon = findViewById(R.id.fast_forward_icon);
         fastRewindIcon = findViewById(R.id.fast_rewind_icon);
-        seekBar = findViewById(R.id.seek_bar);
+
+
+
+        beginingTextViwe.setText("Covered:" + seekBar.getProgress() + "/" + seekBar.getMax());
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progress = 0;
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                progress = i;
+                Toast.makeText(getApplicationContext(), "Changing the progress" , Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(getApplicationContext(), "Started tracking" , Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                beginingTextViwe.setText("Covered: " + progress + "/" +seekBar.getMax());
+                Toast.makeText(getApplicationContext(), "Stopped tracking" , Toast.LENGTH_SHORT).show();
+            }
+        });
 
 
         final AudioManager audioManager = (AudioManager) getApplicationContext().getSystemService(Context.AUDIO_SERVICE);
-        mediaPlayer = MediaPlayer.create(this, R.raw.rafaga_mentirosa);
+            mediaPlayer = MediaPlayer.create(this, R.raw.rafaga_mentirosa);
         mediaPlayer = MediaPlayer.create(this, R.raw.lost_on_you);
 
         playIcon.setOnClickListener(new View.OnClickListener() {
@@ -64,7 +96,7 @@ public class MainActivity extends AppCompatActivity {
         plusIcon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "Increasing the volume", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Increasing the volume...", Toast.LENGTH_SHORT).show();
                 audioManager.adjustVolume(AudioManager.ADJUST_RAISE, AudioManager.FLAG_PLAY_SOUND);
 
             }
@@ -73,7 +105,7 @@ public class MainActivity extends AppCompatActivity {
         minusIcon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "Decreasing the volume", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Decreasing the volume...", Toast.LENGTH_SHORT).show();
                 audioManager.adjustVolume(AudioManager.ADJUST_LOWER, AudioManager.FLAG_PLAY_SOUND);
 
             }
@@ -83,7 +115,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getApplicationContext(), "Fast forwarding", Toast.LENGTH_SHORT).show();
-                mediaPlayer.seekTo(30000);
+                mediaPlayer.seekTo(35000);
 
             }
         });
@@ -91,7 +123,7 @@ public class MainActivity extends AppCompatActivity {
         fastRewindIcon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "Fast rewinding", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Playing from the beginning", Toast.LENGTH_SHORT).show();
                 mediaPlayer.seekTo(0);
 
             }
@@ -100,16 +132,15 @@ public class MainActivity extends AppCompatActivity {
         skipNextIcon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "Skiping to the next song...", Toast.LENGTH_SHORT).show();
-                Toast.makeText(getApplicationContext(), "No other songs :( ", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Skip to the next song...", Toast.LENGTH_SHORT).show();
 
             }
         });
 
-        fastRewindIcon.setOnClickListener(new View.OnClickListener() {
+        skipPreviousIcon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "Fast rewinding...", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Skip to the previous song...", Toast.LENGTH_SHORT).show();
                 mediaPlayer.seekTo(0);
 
             }
